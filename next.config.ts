dd735fccba71c68d71bb13f8f1a471763dd75c57
SAN_DESH63 <99701679+Sandesh2007@users.noreply.github.com>
2025-07-25 07:16:28 +0545

    refactor: simplify next.config.ts by removing unused properties
    - Removed conditional basePath setting for development.
    - Commented out unoptimized images configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-const isDev = process.env.NODE_ENV === 'development';
-
 const nextConfig = {
-  basePath: isDev ? '' : '/nekopress',
-  images: {
-    unoptimized: true,
-  },
+  basePath: '',
+  // images: {
+  //   unoptimized: true,
+  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
